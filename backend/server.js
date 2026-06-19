9072b951e334cb67cb978143242ecd8c3220b65d
phuongwj <phamjulia9@gmail.com>
2026-06-18 22:59:45 -0300

    adjusted server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import router from "./routes/router.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5003;
 
 app.use(cors());
 app.use(express.json());
