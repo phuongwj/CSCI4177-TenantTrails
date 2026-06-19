975b48c621b83817d7112a7c7ee55742bd1b1b07
phuongwj <phamjulia9@gmail.com>
2026-06-18 22:38:14 -0300

    wired up server.js, fixed db.js, added type module to package.json

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
 
+import express from "express";
+import cors from "cors";
+
+import router from "./routes/router.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
